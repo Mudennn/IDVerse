7b46292450a85644a4f7eeb0842fc0d9338426da
Muden <mudennn@yahoo.com>
2023-01-14 00:42:31 +0800

    Upadate form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import './App.css';
-import Form from './components/Form';
+import { ethers } from "ethers";
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
+  const [account, setAccount] = useState(false);
+
+  async function signIn() {
+    const account = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(account[0]);
+  }
+    
   return (
     <div className="App w-full h-full">
       <>
-      <header className='mb-2 px-4 shadow'>
-        <div className='relative mx-auto flex max-w-screen-lg flex-col py-4 sm:flex-row sm:items-center sm:justify-between'>
-          <a className='flex items-center text-2xl font-black' href='/'>IDVerse</a>
-          <div className='peer-checked:block hidden pl-2 py-6 sm:block sm:py-0'>
-            <button className='rounded-xl border-2 border-blue-600 px-6 py-2 font-medium text-blue-600 hover:bg-blue-600 hover:text-white'>Login</button>
+        <header className="mb-2 px-4 shadow">
+          <div className="relative mx-auto flex max-w-screen-lg flex-col py-4 sm:flex-row sm:items-center sm:justify-between">
+            <a className="flex items-center text-2xl font-black" href="/">
+              IDVerse
+            </a>
+            <div className="peer-checked:block hidden pl-2 py-6 sm:block sm:py-0">
+              {account ? (
+                <button className="rounded-xl border-2 border-blue-600 bg-blue-600 px-6 py-2 font-medium text-white ">
+                  Connected
+                </button>
+              ) : (
+                <button
+                  onClick={signIn}
+                  className="rounded-xl border-2 border-blue-600 px-6 py-2 font-medium text-blue-600 hover:bg-blue-600 hover:text-white"
+                >
+                  Log In
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      {/* Digital Data Form  */}
-      <Form />
+        {/* Digital Data Form  */}
+        <Form />
       </>
-
-
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import {useDropzone} from 'react-dropzone'
+
 
 const Form = () => {
+    const [image, setImage] = useState([])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        accept: "image/",
+        onDrop: (acceptedFiles) => {
+            setImage(
+                acceptedFiles.map((uploadFile) => Object.assign(uploadFile, {
+                    preview: URL.createObjectURL(uploadFile)
+                }))
+            )
+        }
+    })
+
   return (
     <section className='max-w-screen-lg mx-auto py-8'>
         <div className='text-left ml-3'>
@@ -38,20 +52,42 @@ const Form = () => {
                 <input type="text" placeholder='43000' className='block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 appearance-none ' />
             </div>
         </div>
-        {/* Gender  */}
+
+        {/* Gender and Email  */}
         <div className='text-left flex mt-3'>
-            <div className='md:w-1/3 px-3 mb-6 md:mb-0'>
+        <div className='md:w-1/2 px-3 mb-6 md:mb-0'>
                 <label htmlFor="" className='mb-1 font-semibold text-gray-500'>Sex</label>
                 <select name="gender" id="" className='block w-full bg-gray-100 border text-gray-700 py-3 px-4 pr-8 rounded'><option value="Male">Male</option><option value="Female">Female</option></select>
             </div>
-        </div>
-        {/* Email */}
-        <div className='text-left mt-3'>
-            <div className='md:w-1/2 px-3'>
+            <div className='md:w-full px-3'>
                 <label htmlFor="" className='mb-1 font-semibold text-gray-500'>Email</label>
                 <input type="text" placeholder='JohnDoe@gmail.com' className='block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 appearance-none ' />
             </div>
         </div>
+            
+        {/* Upload Image  */}
+        <div className='ml-3 mt-3 text-left'>  
+        <label  className='mb-1 font-semibold text-gray-500'>Upload Your IC</label> 
+         <div {...getRootProps()} className='bg-gray-200  p-12 cursor-pointer w-1/2 text-center'>
+            <input {...getInputProps()} />
+             <p>Drag 'n' drop some files here, or click to select files</p>
+         </div>
+         <div>
+             {image.map((uploadFile, index) => {
+                 return (
+                    <>
+                    <div key={index} className='mt-5 text-left'>
+                         <p className='text-gray-500'>Preview</p>
+                         <img src={uploadFile.preview} alt="" className='w-1/2' />
+                         <button onClick={() => setImage([])}>Clear</button>
+                     </div>
+                     
+                     </>
+                 )
+             })}
+         </div>
+         </div>
+         
         {/* Save Button  */}
         <div className='mt-5 ml-3'>
             <button className='my-2 flex w-32 items-center justify-center rounded-lg bg-blue-700 py-2 text-center font-bold text-white outline-none transition focus:ring'>Save</button>
